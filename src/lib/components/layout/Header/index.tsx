0823260a2031ae8d1cd26ecb6f53eb641f5982ab
Julian Jackson <julian@kramphub.com>
2022-03-15 15:17:22 +0100

    jetbrains project file, header tweak

---
+++ src/lib/components/layout/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Box, Text, Avatar} from '../../../ui-components'
+import {NavLink} from "react-router-dom";
+
+
+export const Header = () => {
+
+    return (
+        <header>
+            <Box sx={{
+                display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between', paddingX: 2,
+                paddingY: 1, backgroundColor: '#e0e0e0'
+            }}>
+                <NavLink to='/'>
+                    <Text>Quidle</Text>
+                </NavLink>
+                <Avatar variant="circular"/>
+            </Box>
+        </header>
+    );
+};
